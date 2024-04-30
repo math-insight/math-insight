5be25119dea8137b0cc1aaacde831e61254ab767
dejwodejo <d.drapinski@proton.me>
2024-04-30 18:32:48 +0200

    basic projects descriptions

--- projects.js
+++ projects.js
@@ -1,17 +1,12 @@
 const projects = [
     {
-        title: 'tytuł1',
-        description: 'opis1 Quisque hendrerit, velit faucibus placerat blandit, metus tellus convallis urna, a auctor urna quam vitae lectus. Fusce quis interdum justo, eu aliquam massa. Donec euismod feugiat quam, eget facilisis enim dignissim vitae. Curabitur ultricies tristique faucibus. Nullam nibh neque, maximus sed sem a, egestas semper nisi. Integer id magna sed.',
-        directory: 'project1'
+        title: 'Estymacja poziomu wody w Odrze na wysokości Huty Miedzi Głogów.',
+        description: 'Znalezienie oraz opracowanie danych Instytut Meteorologii i Gospodarki Wodnej. Opracowanie modelu matematycznego estymującego poziom wody na podstawie informacji z stacji poprzedzających Głogów.',
+        path: 'project1'
     },
     {
-        title: 'tytuł2',
-        description: 'opis2 Nulla luctus ipsum ut placerat malesuada. In in risus eu est efficitur molestie. Cras in libero ultricies, vehicula massa ut, accumsan elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus et libero eget orci ullamcorper venenatis. Fusce non dolor ut libero viverra vehicula a.',
+        title: 'Aplikacja webowa ZairyMON',
+        description: 'Aplikacja webowa służąca do monitorowania jakości powietrza i dynamicznego przedstawiania wyników pomiarów oraz symulacji matematycznych. </br><a href="app.zairymon.pl">app.zairymon.pl</a>',
         directory: 'project2'
     },
-    {
-        title: 'tytuł3',
-        description: 'opis3 Morbi condimentum dui sodales, finibus metus ut, rhoncus risus. Duis convallis, dolor ut aliquam tristique, libero tellus molestie arcu, nec sollicitudin metus eros id mi. Maecenas porttitor lectus in dui facilisis suscipit. In eget odio ipsum. Donec bibendum justo a egestas gravida. Donec sed dui magna. Etiam vitae hendrerit justo.',
-        directory: 'project3'
-    }
 ];
