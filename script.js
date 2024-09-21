5fd0c1126dd92f0b4e642031cab19f24862013ec
agatav13 <agataomasta05@gmail.com>
2024-09-21 13:39:30 +0200

    format code

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const textElement = document.getElementById('title');
+const textElement = document.getElementById("title");
 const text = "Koło Naukowe Matematyki Stosowanej Insight.";
 let index = 0;
 
 const allProjects = document.getElementById("all-projects");
 
 function typeText() {
-    if (index < text.length) {
-        textElement.textContent += text.charAt(index);
-        index++;
-        setTimeout(typeText, 100);
-    } else {
-        document.getElementById('caret').style.display = 'none';
-    }
+  if (index < text.length) {
+    textElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeText, 100);
+  } else {
+    document.getElementById("caret").style.display = "none";
+  }
 }
 
 function getProjects() {
-    for (const element of projects) {
-        allProjects.innerHTML += `
-            <div class="project">
-                <h2 class="project-title">${element.title}</h2>
-                <p class="project-description">${element.description}</p>
-            </div>
-            `;
-    }
+  for (const element of projects) {
+    allProjects.innerHTML += `
+      <div class="project">
+        <h2 class="project-title">${element.title}</h2>
+        <p class="project-description">${element.description}</p>
+      </div>
+    `;
+  }
 }
 
 // <div class="button-container">
@@ -30,7 +30,7 @@ function getProjects() {
 // </div>
 
 window.onload = function () {
-    textElement.style.display = "block"
-    typeText();
-    getProjects();
-}
+  textElement.style.display = "block";
+  typeText();
+  getProjects();
+};
