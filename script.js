5db88cb3a6e4a7a71096d0b528cc32e18ba00e03
agatav13 <agataomasta05@gmail.com>
2024-09-21 20:12:34 +0200

    css for mobile and 'see-more' button logic change

--- script.js
+++ script.js
@@ -16,18 +16,21 @@ function typeText() {
 
 function getProjects() {
   for (const element of projects) {
+    const seeMoreButton = element.path
+      ? `<div class="button-container">
+          <button class="see-more-button hover-line" type="button" onClick=location.href='more-about-projects/${element.path}.html'>Zobacz więcej.</button>
+        </div>`
+      : "";
+
     allProjects.innerHTML += `
       <div class="project">
         <h2 class="project-title">${element.title}</h2>
         <p class="project-description">${element.description}</p>
+        ${seeMoreButton}
       </div>
     `;
   }
 }
-
-// <div class="button-container">
-    // <button class="see-more-button hover-line" type="button" onClick=location.href='more-about-projects/${element.path}.html'>Zobacz więcej.</button>
-// </div>
 
 window.onload = function () {
   textElement.style.display = "block";
