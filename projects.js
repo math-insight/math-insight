6d1ad6f5a8cd5ab46c4e02e480ea18bb15d2651b
agatav13 <agataomasta05@gmail.com>
2024-12-14 17:16:45 +0100

    add project pages

--- projects.js
+++ projects.js
@@ -3,12 +3,12 @@ const projects = [
     title: "Estymacja poziomu wody w Odrze na wysokości Huty Miedzi Głogów.",
     description:
       "Znalezienie oraz opracowanie danych Instytut Meteorologii i Gospodarki Wodnej. Opracowanie modelu matematycznego estymującego poziom wody na podstawie informacji z stacji poprzedzających Głogów.",
-    path: "", // wpisać nazwe pliku ("water-level-estimation") jak strona będzie gotowa
+    path: "water-level-estimation", // wpisać nazwe pliku ("water-level-estimation") jak strona będzie gotowa
   },
   {
     title: "Aplikacja webowa ZairyMON.",
     description:
-      "Aplikacja webowa służąca do monitorowania jakości powietrza i dynamicznego przedstawiania wyników pomiarów oraz symulacji matematycznych. </br><a href='https://app.zairymon.pl/'>app.zairymon.pl</a>",
-    path: "", // wpisać nazwe pliku ("zairy-web-app") jak strona będzie gotowa
+      "Aplikacja webowa służąca do monitorowania jakości powietrza i dynamicznego przedstawiania wyników pomiarów oraz symulacji matematycznych. </br><a href='https://app.zairymon.pl/' target='_blank' style='text-decoration: underline;'>app.zairymon.pl</a>",
+    path: "zairy-web-app", // wpisać nazwe pliku ("zairy-web-app") jak strona będzie gotowa
   },
 ];
