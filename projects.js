e0575672363cdd5bac0f8b657a3c0a8f93d75523
agatav13 <agataomasta05@gmail.com>
2025-10-20 18:44:50 +0200

    add lidar-lab project page

--- projects.js
+++ projects.js
@@ -11,4 +11,9 @@ const projects = [
       "Aplikacja webowa służąca do monitorowania jakości powietrza i dynamicznego przedstawiania wyników pomiarów oraz symulacji matematycznych. </br><a href='https://app.zairymon.pl/' target='_blank' style='text-decoration: underline;'>app.zairymon.pl</a>",
     path: "zairy-web-app", // wpisać nazwe pliku ("zairy-web-app") jak strona będzie gotowa
   },
+  {
+    title: "LiDARLab – system pozyskiwania i analizy danych 3D do zastosowań inżynierskich i komercyjnych.",
+    description: "Projekt LiDARLab koncentruje się na opracowaniu zintegrowanego systemu do pozyskiwania, przetwarzania i analizy danych przestrzennych z wykorzystaniem technologii LiDAR i skanerów 3D. Celem projektu jest rozwój narzędzi umożliwiających precyzyjne modelowanie obiektów i przestrzeni w zastosowaniach inżynierskich, przemysłowych oraz komercyjnych – od monitoringu infrastruktury po analizy środowiskowe i logistyczne.",
+    path: "lidar-lab",
+  }
 ];
