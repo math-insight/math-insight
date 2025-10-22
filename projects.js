b1578c76880394a81fcc790f70c8f8c50d039b02
agatav13 <agataomasta05@gmail.com>
2025-10-22 13:43:01 +0200

    change lidar-lab page text

--- projects.js
+++ projects.js
@@ -1,5 +1,10 @@
 const projects = [
   {
+    title: "LiDARLab – system pozyskiwania i analizy danych 3D do zastosowań inżynierskich i komercyjnych.",
+    description: "Celem projektu realizowanego przez nasz zespół jest opracowanie innowacyjnego systemu do skanowania i analizy przestrzeni 3D z wykorzystaniem technologii LiDAR oraz autorskiego oprogramowania analitycznego. Rozwiązanie umożliwi szybkie i precyzyjne tworzenie cyfrowych modeli rzeczywistych obiektów.",
+    path: "lidar-lab",
+  },
+  {
     title: "Estymacja poziomu wody w Odrze na wysokości Huty Miedzi Głogów.",
     description:
       "Znalezienie oraz opracowanie danych Instytut Meteorologii i Gospodarki Wodnej. Opracowanie modelu matematycznego estymującego poziom wody na podstawie informacji z stacji poprzedzających Głogów.",
@@ -11,9 +16,4 @@ const projects = [
       "Aplikacja webowa służąca do monitorowania jakości powietrza i dynamicznego przedstawiania wyników pomiarów oraz symulacji matematycznych. </br><a href='https://app.zairymon.pl/' target='_blank' style='text-decoration: underline;'>app.zairymon.pl</a>",
     path: "zairy-web-app", // wpisać nazwe pliku ("zairy-web-app") jak strona będzie gotowa
   },
-  {
-    title: "LiDARLab – system pozyskiwania i analizy danych 3D do zastosowań inżynierskich i komercyjnych.",
-    description: "Projekt LiDARLab koncentruje się na opracowaniu zintegrowanego systemu do pozyskiwania, przetwarzania i analizy danych przestrzennych z wykorzystaniem technologii LiDAR i skanerów 3D. Celem projektu jest rozwój narzędzi umożliwiających precyzyjne modelowanie obiektów i przestrzeni w zastosowaniach inżynierskich, przemysłowych oraz komercyjnych – od monitoringu infrastruktury po analizy środowiskowe i logistyczne.",
-    path: "lidar-lab",
-  }
 ];
